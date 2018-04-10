2589678ee26c94a5b37abc91695f6d1188902543
amit vishwakarma <amitvishwakarma2625@gmail.com>
2018-04-10 16:55:45 +0530

    Basic R program - matrix, dataframe colors

--- Work-2.R
+++ Work-2.R
@@ -8,3 +8,58 @@ D[indexes,]
 
 D[rev(order(D$y)),]
 
+n = c(2, 3, 5) 
+s = c("aa", "bb", "cc") 
+b = c(TRUE, FALSE, TRUE) 
+df = data.frame(n, s, b)  
+df
+
+
+colors()[c(552,254,26)]
+grep("red",colors())
+colors()[grep("red",colors())]
+
+
+colors()[grep("sky",colors())]
+
+
+# Create two vectors of different lengths.
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+
+# Take these vectors as input to the array.
+result <- array(c(vector1,vector2),dim = c(3,3,2))
+print(result)
+
+# Create two vectors of different lengths.
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+column.names <- c("COL1","COL2","COL3")
+row.names <- c("ROW1","ROW2","ROW3")
+matrix.names <- c("Matrix1","Matrix2")
+
+# Take these vectors as input to the array.
+result <- array(c(vector1,vector2),dim = c(3,3,2),dimnames = list(row.names,column.names,
+                                                                  matrix.names))
+print(result)
+
+# Create two vectors of different lengths.
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+column.names <- c("COL1","COL2","COL3")
+row.names <- c("ROW1","ROW2","ROW3")
+matrix.names <- c("Matrix1","Matrix2")
+
+# Take these vectors as input to the array.
+result <- array(c(vector1,vector2),dim = c(3,3,2),dimnames = list(row.names,
+                                                                  column.names, matrix.names))
+
+# Print the third row of the second matrix of the array.
+print(result[3,,2])
+
+# Print the element in the 1st row and 3rd column of the 1st matrix.
+print(result[1,3,1])
+
+# Print the 2nd Matrix.
+print(result[,,2])
+
