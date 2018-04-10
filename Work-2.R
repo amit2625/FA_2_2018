97827ff78057f13107397ed220ee05571146c5b2
amit vishwakarma <amitvishwakarma2625@gmail.com>
2018-04-10 15:13:15 +0530

    This is 3rd Commit.

--- Work-2.R
+++ Work-2.R
@@ -2,3 +2,14 @@
 
 a <- rnorm(100)
 b <- rnorm(100)
+
+
+print(sample(c(2,5,3), size=3, replace=FALSE))
+print(sample(c(2,5,3), size=3, replace=TRUE))
+barplot(table(sample(1:3, size=1000, replace=TRUE, prob=c(.30,.60,.10))))
+
+
+
+tbl <- read.table(file.choose(),header=TRUE,sep=",")
+population <- tbl["POPESTIMATE2009"]
+print(summary(population[-1:-5,]))
